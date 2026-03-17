2fd52e9b6bfb97df5b3b339f8ab94f20297bc868
tarunachoudhary43-hash <tarunachoudhary43@gmail.com>
2026-03-17 14:55:17 -0400

    .

--- Ayurveda/src/main.tsx
+++ Ayurveda/src/main.tsx
@@ -1,10 +1,4 @@
 import ReactDOM from "react-dom/client";
-</xai:function_call;
-
-
-
-<xai:function_call name="edit_file">
-<parameter name="path">Ayurveda/src/Components/About.tsx
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
